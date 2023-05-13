e928529dab09c0770df39572cc4a2b2e7140618c
GadeliaGoderdzi <130245892+GadeliaGoderdzi@users.noreply.github.com>
2023-05-13 13:23:17 -0400

    club

---
+++ bookclub.py
@@ -0,0 +1,13 @@
+import streamlit as st
+#----------------------------------text editing------------------------------------
+club = open("Norwegian Book Club 100.csv")
+author = []
+name = []
+
+for book in club:
+    book = book.split("by")
+    author.append(book[1])
+    name.append(book[0])
+
+st.wrtite(author)
+st.wrtite(name)
