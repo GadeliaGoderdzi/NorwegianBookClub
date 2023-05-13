4c52f93ca63fe60fd36ee4f8224e1977fa442e5c
GadeliaGoderdzi <130245892+GadeliaGoderdzi@users.noreply.github.com>
2023-05-13 13:27:35 -0400

    Update bookclub.py

--- bookclub.py
+++ bookclub.py
@@ -3,12 +3,14 @@ import streamlit as st
 club = open("Norwegian Book Club 100.csv")
 author = []
 name = []
+cluber = st.container()
 
-for book in club:
+for book in club[1:]:
     book = book.split("by")
     st.write(book)
     author.append(book[1])
     name.append(book[0])
 
-st.wrtite(author)
-st.wrtite(name)
+with cluber:
+    st.wrtite(author)
+    st.wrtite(name)
