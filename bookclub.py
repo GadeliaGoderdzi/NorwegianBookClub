b8a819e3971e64d139d68ef4a321f9c84ca9805e
GadeliaGoderdzi <130245892+GadeliaGoderdzi@users.noreply.github.com>
2023-05-13 18:59:47 -0400

    Update bookclub.py

--- bookclub.py
+++ bookclub.py
@@ -4,13 +4,16 @@ club = open("Norwegian Book Club 100.csv")
 author = []
 name = []
 cluber = st.container()
-
-for book in club[1:100]:
-    book = book.split("by")
-    st.write(book)
-    author.append(book[1])
-    name.append(book[0])
+list_files = []
+for book in club:
+    list_files.append(book)
+# for book in club:
+#     book = book.split("by")
+#     st.write(book)
+#     author.append(book[1])
+#     name.append(book[0])
 
 with cluber:
+    st.write(list_files)
     st.wrtite(author)
     st.wrtite(name)
